8d5c3b94fc1f9c811e4398c16b20b68c08f62afe
FatimatusZahroh <zahrohfatimatus136@gmail.com>
2026-03-20 08:16:18 +0700

    pertama

---
+++ android/app/src/main/kotlin/com/example/mobile_bangking/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mobile_bangking
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: BriMoFinalReplication(),
+    debugShowCheckedModeBanner: false,
+  ))
+}
+
+class BriMoFinalReplication extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFF4F7FA),
+      // -----NAVIGATION BAWAH LENGKAP----
+    )
+  }
+}
