10181e9a5ef3137e4c675bcf3b5b0f8330492e8a
FatimatusZahroh <zahrohfatimatus136@gmail.com>
2026-03-20 10:28:12 +0700

    pertama

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,78 @@ class BriMoFinalReplication extends StatelessWidget {
             clipBehavior: Clip.none,
             children: [
               // 1. HEADER BIRU
-            ]
+              Container(
+                height: 220,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Aligment.topCenter,
+                    end: Aligment.bottomCenter,
+                    colors: [Color(0xFF004D8C), Color(0xFF0074D9)],
+                  ),
+                ),
+                padding: EdgeInsets.only(top: 60, left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAligment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Selamat Siang,", style: TextStyle(color: Colors.white70, fontSize: 14)),
+                        Text("Fatimatus", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold)),
+                      ],
+                    ),
+                    Icon(Icons.headset_mic_outlined, color: Colors.white),
+                  ],
+                ),
+              ),
+              // 2.KOTAK PUTIH 1 (SALDO & TRANSFER JADI SATU)
+              Positioned(
+                top: 130,
+                left: 15,
+                right: 15,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [BoxShadow(color: Colors.black12,blurRadius: 5)],
+                  ),
+                  child: Column(
+                    children: [
+                      _buildBlueSaldoCard(),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 15),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            _buildTopMenu(Icons.send, "Transfer", Colors.blue),
+                            _buildTopMenu(Icons.receipt, "BRIVA", Colors.teal),
+                            _buildTopMenu(Icons.water_drop, "PDAM", Colors.blueAccent),
+                            _buildTopMenu(Icons.phone_android, "Pulsa/Data", Colors.green),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          SizedBox(height: 180), //Jarak Aman
+
+          // 3.KOTAK PUTIH 2 (PROMO WARNA BIRU)
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+            child: Container(
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [Color(0xFF00529C), Color(0xFF0074D9)]),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              
+            )
           )
         )
       )
