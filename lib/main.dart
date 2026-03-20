c0ddd9e03f7085cec17d4063b010a609fad06938
FatimatusZahroh <zahrohfatimatus136@gmail.com>
2026-03-20 08:56:41 +0700

    pertama

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,40 @@ class BriMoFinalReplication extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xFFF4F7FA),
       // -----NAVIGATION BAWAH LENGKAP----
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Color(0xFF0074D9),
+        shape: CircleBorder(),
+        child: Icon(Icon,qr_code_scanner,color: Colors.white, size: 30),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRecttangel(),
+        notchMargin: 8,
+        child: Container(
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              _buildBottomIcon(Icons.home, "Home", true),
+              _buildBottomIcon(Icons.syn_alt, "Mutasi", false),
+              SizedBox(width: 40),
+              _buildBottomIcon(Icons.notifications_none, "Aktivitas", false),
+              _buildBottomIcon(Icons.person_outline, "Akun", false),
+            ],
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          Stack(
+            clipBehavior: Clip.none,
+            children: [
+              // 1. HEADER BIRU
+            ]
+          )
+        )
+      )
     )
   }
 }
