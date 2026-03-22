761eb7cf27dd1e5cf4f7d7f2a45c683b5bf8bc1c
FatimatusZahroh <zahrohfatimatus136@gmail.com>
2026-03-22 18:06:57 +0700

    pertama

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() {
   runApp(MaterialApp(
     home: BriMoFinalReplication(),
     debugShowCheckedModeBanner: false,
-  ))
+  ));
 }
 
 class BriMoFinalReplication extends StatelessWidget {
@@ -18,10 +18,10 @@ class BriMoFinalReplication extends StatelessWidget {
         onPressed: () {},
         backgroundColor: Color(0xFF0074D9),
         shape: CircleBorder(),
-        child: Icon(Icon,qr_code_scanner,color: Colors.white, size: 30),
+        child: Icon(Icons.qr_code_scanner, color: Colors.white, size: 30),
       ),
       bottomNavigationBar: BottomAppBar(
-        shape: CircularNotchedRecttangel(),
+        shape: CircularNotchedRectangle(),
         notchMargin: 8,
         child: Container(
           height: 60,
@@ -29,7 +29,7 @@ class BriMoFinalReplication extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               _buildBottomIcon(Icons.home, "Home", true),
-              _buildBottomIcon(Icons.syn_alt, "Mutasi", false),
+              _buildBottomIcon(Icons.sync_alt, "Mutasi", false),
               SizedBox(width: 40),
               _buildBottomIcon(Icons.notifications_none, "Aktivitas", false),
               _buildBottomIcon(Icons.person_outline, "Akun", false),
@@ -49,14 +49,14 @@ class BriMoFinalReplication extends StatelessWidget {
                 width: double.infinity,
                 decoration: BoxDecoration(
                   gradient: LinearGradient(
-                    begin: Aligment.topCenter,
-                    end: Aligment.bottomCenter,
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
                     colors: [Color(0xFF004D8C), Color(0xFF0074D9)],
                   ),
                 ),
                 padding: EdgeInsets.only(top: 60, left: 20, right: 20),
                 child: Row(
-                  mainAxisAlignment: MainAxisAligment.spaceBetween,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Column(
@@ -181,15 +181,82 @@ class BriMoFinalReplication extends StatelessWidget {
                 _buildGridItem(Icons.swap_horiz, "Transfer", Colors.blue),
                 _buildGridItem(Icons.shopping_bag, "Lifestyle", Colors.pink),
                 _buildGridItem(Icons.atm, "Setor & Tarik", Colors.blue),
-                _buildGridItem(Icons.bar_cart, "Investasi", Colors.orange),
+                _buildGridItem(Icons.bar_chart, "Investasi", Colors.orange),
                 _buildGridItem(Icons.more_horiz, "Lainnya", Colors.grey),
               ],
             ),
           ),  
           SizedBox(height: 20),
-          ]          
-        )
-      )
-    )
+          ],          
+        ),
+      ),
+    );
+  }
+
+  //---WIDGET HELPRES---
+
+  Widget _buildBlueSaldoCard() {
+    return Container(
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(15),
+      decoration: BoxDecoration(color: Color(0xFF004A8E), borderRadius: BorderRadius.circular(12)),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Saldo Rekening Utama", style: TextStyle(color: Colors.white70, fontSize: 11)),
+                  Text("Rp.135.000", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold)),
+
+                ],
+              ),
+              Icon(Icons.visibility_off, color: Colors.white70, size: 20),
+            ],
+          ),
+          Divider(color: Colors.white24, height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [Text("Semua Rekeningmu", style: TextStyle(color: Colors.white, fontSize: 12)), Icon(Icons.chevron_right, color: Colors.white, size: 18)],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTopMenu(IconData icon, String label, Color color) {
+    return Column(
+      children: [
+        Icon(icon, color: color, size: 26),
+        SizedBox(height: 4),
+        Text(label, style: TextStyle(fontSize: 10, fontWeight: FontWeight.w500)),
+      ],
+    );
+  }
+
+  Widget _buildGridItem(IconData icon, String label, Color color) {
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(color: color.withOpacity(0.1), shape: BoxShape.circle),
+          child: Icon(icon, color: color, size: 22),
+        ),
+        SizedBox(height: 4),
+        Text(label, textAlign: TextAlign.center, style: TextStyle(fontSize: 9, fontWeight: FontWeight.w500)),
+      ],
+    );
+  }
+
+  Widget _buildBottomIcon(IconData icon, String label, bool isActive) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(icon, color: isActive ? Colors.blue : Colors.grey, size: 24),
+        Text(label, style: TextStyle(color: isActive ? Colors.blue : Colors.grey, fontSize: 10)),
+      ],
+    );
   }
 }
